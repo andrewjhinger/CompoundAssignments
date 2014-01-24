2644a0194e9431884541b285bee34aef80ac5bfc
ahinger <andrewjhinger@gmail.com>
2014-01-24 11:03:24 -0500

    First Commit

---
+++ CompoundAssignments/CompoundAssignments.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CompoundAssignments
+{
+    partial class CompoundAssignments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.seetingsGroupBox = new System.Windows.Forms.GroupBox();
+            this.letterLabel = new System.Windows.Forms.Label();
+            this.letterTextBox = new System.Windows.Forms.TextBox();
+            this.gridSizeComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.drawGroupBox = new System.Windows.Forms.GroupBox();
+            this.seetingsGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // seetingsGroupBox
+            // 
+            this.seetingsGroupBox.Controls.Add(this.letterLabel);
+            this.seetingsGroupBox.Controls.Add(this.letterTextBox);
+            this.seetingsGroupBox.Controls.Add(this.gridSizeComboBox);
+            this.seetingsGroupBox.Controls.Add(this.label1);
+            this.seetingsGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.seetingsGroupBox.Name = "seetingsGroupBox";
+            this.seetingsGroupBox.Size = new System.Drawing.Size(268, 62);
+            this.seetingsGroupBox.TabIndex = 0;
+            this.seetingsGroupBox.TabStop = false;
+            // 
+            // letterLabel
+            // 
+            this.letterLabel.AutoSize = true;
+            this.letterLabel.Location = new System.Drawing.Point(121, 24);
+            this.letterLabel.Name = "letterLabel";
+            this.letterLabel.Size = new System.Drawing.Size(37, 13);
+            this.letterLabel.TabIndex = 3;
+            this.letterLabel.Text = "Letter:";
+            // 
+            // letterTextBox
+            // 
+            this.letterTextBox.Location = new System.Drawing.Point(162, 21);
+            this.letterTextBox.Name = "letterTextBox";
+            this.letterTextBox.Size = new System.Drawing.Size(100, 20);
+            this.letterTextBox.TabIndex = 2;
+            this.letterTextBox.TextChanged += new System.EventHandler(this.letterTextBox_TextChanged);
+            // 
+            // gridSizeComboBox
+            // 
+            this.gridSizeComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.gridSizeComboBox.FormattingEnabled = true;
+            this.gridSizeComboBox.Items.AddRange(new object[] {
+            "4",
+            "10",
+            "20"});
+            this.gridSizeComboBox.Location = new System.Drawing.Point(64, 21);
+            this.gridSizeComboBox.Name = "gridSizeComboBox";
+            this.gridSizeComboBox.Size = new System.Drawing.Size(51, 21);
+            this.gridSizeComboBox.TabIndex = 1;
+            this.gridSizeComboBox.SelectedIndexChanged += new System.EventHandler(this.gridSizeComboBox_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Grid Size:";
+            // 
+            // drawGroupBox
+            // 
+            this.drawGroupBox.Location = new System.Drawing.Point(12, 95);
+            this.drawGroupBox.Name = "drawGroupBox";
+            this.drawGroupBox.Size = new System.Drawing.Size(268, 166);
+            this.drawGroupBox.TabIndex = 1;
+            this.drawGroupBox.TabStop = false;
+            this.drawGroupBox.Paint += new System.Windows.Forms.PaintEventHandler(this.drawGroupBox_Paint);
+            // 
+            // CompoundAssignments
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.drawGroupBox);
+            this.Controls.Add(this.seetingsGroupBox);
+            this.Name = "CompoundAssignments";
+            this.Text = "CompoundAssignments";
+            this.seetingsGroupBox.ResumeLayout(false);
+            this.seetingsGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox seetingsGroupBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox drawGroupBox;
+        private System.Windows.Forms.ComboBox gridSizeComboBox;
+        private System.Windows.Forms.Label letterLabel;
+        private System.Windows.Forms.TextBox letterTextBox;
+    }
+}
+
---
+++ CompoundAssignments/CompoundAssignments.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CompoundAssignments
+{
+    public partial class CompoundAssignments : Form
+    {
+        public CompoundAssignments()
+        {
+            InitializeComponent();
+            MessageBox.Show("Enter a letter into the textbox");
+
+            // Select the first item in the combo box.
+            gridSizeComboBox.SelectedIndex = 0;
+
+            // Prefix
+            string prefixValues = "0 ";
+            int result = 0;
+            int value = 1;
+            for (int i = 1; i <= 4; i++)
+            {
+                result += ++value;
+                prefixValues += result + " ";
+            }
+
+            // Postfix
+            string postfixValues = "0 ";
+            result = 0;
+            value = 1;
+            for (int i = 1; i <= 4; i++)
+            {
+                result += value++;
+                postfixValues += result + " ";
+            }
+
+            Console.WriteLine("Prefix: " + prefixValues);
+            Console.WriteLine("Postfix: " + postfixValues);
+
+        }
+
+        private void DrawGrid(Graphics drawingArea)
+        {
+             float gridMargin = 10.0F;       // Margin to keep grid away from group box edge.
+            int gridSize = 0;               // Grid size, i.e. 4x4, 10x10, etc.
+            
+            // Try to convert the grid size in the combo box to an integer.
+            if (Int32.TryParse(gridSizeComboBox.Text, out gridSize))
+            {
+                // Make sure we have a grid size > 0 before we attempt to draw grid.
+                if (gridSize > 0)
+                {
+                    // Create a pen object for drawing.
+                    Pen redPen = new Pen(Color.Red, 2);
+                    Pen bluePen = new Pen(Color.Blue, 2);
+                    
+                    // Use the smaller of the drawing area width or height to ensure a uniform grid spacing.
+                    float gridWidth = drawingArea.VisibleClipBounds.Width;
+                    if (drawingArea.VisibleClipBounds.Height < gridWidth)
+                        gridWidth = drawingArea.VisibleClipBounds.Height;
+                    
+                    // Subtract the margin value.
+                    gridWidth -= gridMargin * 2;
+                    
+                    // Determine grid cell width.
+                    float cellWidth = gridWidth / gridSize;
+                    
+                    // Set up starting x and y coordinates for both horizontal and vertical lines.
+                    float horizontalX = gridMargin;
+                    float horizontalY = 0;
+                    float verticalX = 0;
+                    float verticalY = gridMargin;
+                    
+                    // The number of lines in grid is equal to the size of the grid plus 1, so loop over size of grid + 1,
+                    // drawing horizontal and vertical lines.
+                    for (int i = 0; i < gridSize + 1; i++)
+                    {
+                        // Select pen, red for even, blue for odd.
+                        Pen pen = bluePen;
+                        if (i % 2 == 0)
+                            pen = redPen;
+
+                        // Draw horizontal lines.
+                        Console.WriteLine("Horizontal: ({0},{1}) to ({2},{3})", horizontalX, horizontalY + gridMargin, horizontalX + gridWidth, horizontalY + gridMargin);
+                        drawingArea.DrawLine(pen, horizontalX, horizontalY + gridMargin, horizontalX + gridWidth, horizontalY + gridMargin);
+                        horizontalY += cellWidth;
+                        
+                        // Draw vertical lines.
+                        Console.WriteLine("Vertical: ({0},{1}) to ({2},{3})", verticalX + gridMargin, verticalY, verticalX + gridMargin, verticalY + gridWidth);
+                        drawingArea.DrawLine(pen, verticalX + gridMargin, verticalY, verticalX + gridMargin, verticalY + gridWidth);
+                        verticalX += cellWidth;
+
+                        if (i < gridSize)
+                        {
+                            float x1 = horizontalX;
+                            for (int b = 0; b < gridSize; b++)
+                            {
+                                drawLetter(drawingArea, x1, horizontalY - gridMargin, gridSize);
+                                x1 += cellWidth;
+                            }
+                        }
+
+
+                    }
+                }
+            }
+        }
+
+        private void drawLetter(System.Drawing.Graphics drawingArea, float x, float y, int gridSize)
+        {
+
+            //int gridSize = 0;               // Grid size, i.e. 4x4, 10x10, etc.
+            Brush blackBrush = Brushes.Black;
+            Font timesFont = new Font("Times New Roman", 12.0f);
+            string letter = "A";
+
+            if (letterTextBox.Text != "")
+            {
+                letter = letterTextBox.Text;
+                if (gridSizeComboBox.SelectedIndex == gridSizeComboBox.FindStringExact("4"))
+                {
+                    timesFont = new Font("Times New Roman", 12.0f);
+                }
+
+                if (gridSizeComboBox.SelectedIndex == gridSizeComboBox.FindStringExact("10"))
+                {
+                    timesFont = new Font("Times New Roman", 12.0f -4.0f);
+                    
+                }
+                if (gridSizeComboBox.SelectedIndex == gridSizeComboBox.FindStringExact("20"))
+                {
+                    timesFont = new Font("Times New Roman", 12.0f -9.0f);
+                    
+                }
+                drawingArea.DrawString(letter, timesFont, blackBrush, new PointF(x, y));
+
+
+            }
+        }
+
+        private void gridSizeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Notify the group box control that it should redraw itself, which will call the Paint event below.
+            drawGroupBox.Refresh();
+        }
+
+        private void drawGroupBox_Paint(object sender, PaintEventArgs e)
+        {
+            // Call the base class OnPaint event to ensure any other controls that need to be redrawn are also redrawn.
+            base.OnPaint(e);
+            // Call our draw grid method.
+            DrawGrid(e.Graphics);
+           
+        }
+
+        private void letterTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // Notify the group box control that it should redraw itself, which will call the Paint event below.
+            drawGroupBox.Refresh();
+        }
+
+        
+            
+        }
+}
+      
+
+
+
